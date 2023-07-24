18df38497c0e8109628920ee8a7cf72ee48b4bc5
RaevschiCatalin <raevschicatalin@gmail.com>
2023-07-24 17:40:45 +0300

    initialised next,tailwind,postcss,typescript

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold text-center">Searching for a code buddy?</h1>
+      <p className="text-2xl text-center">We got you covered!</p>
+    </main>
+  )
+}
