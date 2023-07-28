b7c96be5fcf9dbea5be3646ca2cf211e1817a26f
RaevschiCatalin <raevschicatalin@gmail.com>
2023-07-28 16:01:05 +0300

    fix bug with the main screen button/started to implement good reg form, not finished/looks horible

--- screens/SignUp.tsx
+++ screens/SignUp.tsx
@@ -46,47 +46,53 @@ const SignUp: React.FC = () => {
   const router = useRouter();
 
   return (
-    <div>
-      <p>Register</p>
-      <form onSubmit={handleSubmit}>
-        <input
-          required
-          name="email"
-          placeholder="email"
-          type="email"
-          onChange={handleChange}
-        />
-        <br />
-        <input
-          required
-          name="username"
-          placeholder="username"
-          type="text"
-          onChange={handleChange}
-        />
-        <br />
-        <input
-          required
-          name="password"
-          placeholder="password"
-          type="password"
-          onChange={handleChange}
-        />
-        <br />
-        <input
-          required
-          name="confirmPassword"
-          placeholder="Confirm password"
-          type="password"
-          onChange={handleChange}
-        />
-        <br />
-
-        <input type="submit" />
-      </form>
-
-      {(error || fbError) && <p>{error || fbError?.message}</p>}
+    <div className="flex justify-center items-center p-24">
+  <form
+    onSubmit={handleSubmit}
+    className="flex flex-col gray_gradient_bg justify-center items-center text-center border-solid rounded-lg p-10 border-2 border-black mt-6 w-1/2"
+  >
+    <h1 className="text-3xl font-extrabold  text-black my-6">Register</h1>
+    <div className="p-4 rounded-lg ">
+      <input
+        className="form_input w-full mb-4"
+        required
+        name="email"
+        placeholder="Email"
+        type="email"
+        onChange={handleChange}
+      />
+      <input
+        required
+        className="form_input mb-4"
+        name="username"
+        placeholder="Username"
+        type="text"
+        onChange={handleChange}
+      />
+      <input
+        className="form_input mb-4"
+        required
+        name="password"
+        placeholder="Password"
+        type="password"
+        onChange={handleChange}
+      />
+      <input
+        className="form_input mb-4"
+        required
+        name="confirmPassword"
+        placeholder="Confirm password"
+        type="password"
+        onChange={handleChange}
+      />
     </div>
+
+    <input type="submit" className="black_btn mt-4" />
+  </form>
+
+  {(error || fbError) && <p>{error || fbError?.message}</p>}
+</div>
+
   );
 };
 
